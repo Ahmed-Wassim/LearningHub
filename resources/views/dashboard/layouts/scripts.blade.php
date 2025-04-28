ba7400d343a0799870565ff85a1ae249ee9d7ada
Ahmed Wassim <wassim@Ahmed-Wassim.>
2025-04-28 11:23:36 +0300

    Working with Dashboard Layouts

---
+++ resources/views/dashboard/layouts/scripts.blade.php
@@ -0,0 +1,15 @@
+    <!-- Javascript -->
+    <script src="{{ asset('dashboard/light/assets/bundles/libscripts.bundle.js') }}"></script>
+
+    <script src="{{ asset('dashboard/light/assets/bundles/vendorscripts.bundle.js') }}"></script>
+
+
+    <script src="{{ asset('dashboard/light/assets/bundles/chartist.bundle.js') }}"></script>
+
+    <script src="{{ asset('dashboard/light/assets/bundles/knob.bundle.js') }}"></script>
+    <!-- Jquery Knob-->
+    <script src="../assets/vendor/toastr/toastr.js"></script>
+
+    <script src="{{ asset('dashboard/light/assets/bundles/mainscripts.bundle.js') }}"></script>
+
+    <script src="{{ asset('dashboard/light/assets/js/index.js') }}"></script>
